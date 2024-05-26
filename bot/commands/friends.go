9a82d867bd9c87c56dff7a66e80abd0616e2ab14
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-05-25 21:53:28 -0500

    Add placeholder

--- bot/commands/friends.go
+++ bot/commands/friends.go
@@ -8,3 +8,5 @@ func FindFriends(session *discordgo.Session, message *discordgo.MessageCreate) {
 	session.ChannelMessageSend(message.ChannelID, "What interest tags are relevant to you? Reply with tags.")
 	// need to fill out
 }
+
+// Will need to think about managing state, if it there is a better way
